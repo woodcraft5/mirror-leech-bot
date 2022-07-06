44d11db8efd830fe34bac7baa8a14f264a740df8
woodcraft5 <sekhfaruk5@gmail.com>
2022-07-06 22:09:01 +0530

    __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -453,47 +453,34 @@ try:
 except:
     CRYPT = None
 try:
-    UNIFIED_EMAIL = getConfig('UNIFIED_EMAIL')
-    if len(UNIFIED_EMAIL) == 0:
+    APPDRIVE_EMAIL = getConfig('APPDRIVE_EMAIL')
+    APPDRIVE_PASS = getConfig('APPDRIVE_PASS')
+    if len(APPDRIVE_EMAIL) == 0 or len(APPDRIVE_PASS) == 0:
         raise KeyError
-except:
-    UNIFIED_EMAIL = None
+except KeyError:
+    APPDRIVE_EMAIL = None
+    APPDRIVE_PASS = None
 try:
-    UNIFIED_PASS = getConfig('UNIFIED_PASS')
-    if len(UNIFIED_PASS) == 0:
-        raise KeyError
+    FSUB = getConfig('FSUB')
+    FSUB = FSUB.lower() == 'true'
 except:
-    UNIFIED_PASS = None
+    FSUB = False
+    LOGGER.info("Force Subscribe is disabled")
 try:
-    HUBDRIVE_CRYPT = getConfig('HUBDRIVE_CRYPT')
-    if len(HUBDRIVE_CRYPT) == 0:
+    CHANNEL_USERNAME = getConfig("CHANNEL_USERNAME")
+    if len(CHANNEL_USERNAME) == 0:
         raise KeyError
-except:
-    HUBDRIVE_CRYPT = None
+except KeyError:
+    log_info("CHANNEL_USERNAME not provided! Using default @woodcraft_repo")
+    CHANNEL_USERNAME = "woodcraft_repo"
 try:
-    KATDRIVE_CRYPT = getConfig('KATDRIVE_CRYPT')
-    if len(KATDRIVE_CRYPT) == 0:
+    FSUB_CHANNEL_ID = getConfig("FSUB_CHANNEL_ID")
+    if len(FSUB_CHANNEL_ID) == 0:
         raise KeyError
-except:
-    KATDRIVE_CRYPT = None
-try:
-    DRIVEFIRE_CRYPT = getConfig('DRIVEFIRE_CRYPT')
-    if len(DRIVEFIRE_CRYPT) == 0:
-        raise KeyError
-except:
-    DRIVEFIRE_CRYPT = None
-try:
-    XSRF_TOKEN = getConfig('XSRF_TOKEN')
-    if len(XSRF_TOKEN) == 0:
-        raise KeyError
-except:
-    XSRF_TOKEN = None
-try:
-    laravel_session = getConfig('laravel_session')
-    if len(laravel_session) == 0:
-        raise KeyError
-except:
-    laravel_session = None
+    FSUB_CHANNEL_ID = int(FSUB_CHANNEL_ID)
+except KeyError:
+    log_info("CHANNEL_ID not provided! Using default id of @woodcraft_repo")
+    FSUB_CHANNEL_ID = -1001115426030
 try:
     BOT_PM = getConfig('BOT_PM')
     BOT_PM = BOT_PM.lower() == 'true'
