4a557296424d8acaf83ce344c2cd460a7412656e
woodcraft5 <sekhfaruk5@gmail.com>
2022-07-13 01:51:00 +0530

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -40,8 +40,8 @@ def stats(update, context):
             f'<b>🌼 Used➤:</b> <code>{used}</code> | <b>🌼 Free➤:</b> <code>{free}</code>\n'\
             f'<b>🌼 T-Up➤:</b> <code>{sent}</code> | <b>🌼 T-Dn➤:</b> <code>{recv}</code>\n'\
             f'<b>🌼 CPU Usage➤:</b> <code>{cpuUsage}</code>% | <b>🌼 RAM Usage➤:</b> <code>{mem_p}%</code>\n'
-    heroku = getHerokuDetails(HEROKU_API_KEY, HEROKU_APP_NAME)
-    if heroku: stats += heroku
+    if heroku := getHerokuDetails(HEROKU_API_KEY, HEROKU_APP_NAME):
+        stats += heroku
     sendMessage(stats, context.bot, update.message)
 
 def start(update, context):
@@ -185,8 +185,7 @@ def bot_help(update, context):
 def main():
     start_cleanup()
     if INCOMPLETE_TASK_NOTIFIER and DB_URI is not None:
-        notifier_dict = DbManger().get_incomplete_tasks()
-        if notifier_dict:
+        if notifier_dict := DbManger().get_incomplete_tasks():
             for cid, data in notifier_dict.items():
                 if ospath.isfile(".restartmsg"):
                     with open(".restartmsg") as f:
