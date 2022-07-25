f51ced415069b889fb66167237c6f4ee9656af15
woodcraft5 <sekhfaruk5@gmail.com>
2022-07-26 04:36:33 +0530

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -163,7 +163,7 @@ help_string_telegraph = f'''<br>
 '''
 
 help = telegraph.create_page(
-        title='WOODcraft Torrent Search',
+        title='WOODcraft Drive Search',
         content=help_string_telegraph,
     )["path"]
 
