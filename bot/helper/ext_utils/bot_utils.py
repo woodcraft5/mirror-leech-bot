8b01a3d2c87e8d0f199f5157cf98c964a9bdc73e
woodcraft5 <sekhfaruk5@gmail.com>
2022-07-26 01:53:33 +0530

    bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -141,7 +141,7 @@ def get_readable_message():
                 globals()['COUNT'] -= STATUS_LIMIT
                 globals()['PAGE_NO'] -= 1
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
-            msg += f"<b>➦★ ✤┅┅●❬ {TITLE_NAME} ❭●┅┅✤</b>"
+            msg += f"<b>➦ ❬ ✤{TITLE_NAME} ❭✤</b>"
             msg += f"<b>  </b>\n"
             msg += f"\n\n<b>➦ File Name:</b> <code>{escape(str(download.name()))}</code>"
             msg += f"\n<b>➦ Status:</b> <i>{download.status()}</i>"
