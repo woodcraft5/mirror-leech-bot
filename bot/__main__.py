6e802b8e2b8bdb8135b901de63729d98e5488343
woodcraft5 <sekhfaruk5@gmail.com>
2022-07-27 21:02:02 +0530

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -197,9 +197,9 @@ def main():
                 if ospath.isfile(".restartmsg"):
                     with open(".restartmsg") as f:
                         chat_id, msg_id = map(int, f)
-                    msg = 'Restarted successfully!'
+                    msg = '✔️Restarted successfully!'
                 else:
-                    msg = 'Bot Restarted!'
+                    msg = '✔️Bot Restarted!'
                 for tag, links in data.items():
                      msg += f"\n\n{tag}: "
                      for index, link in enumerate(links, start=1):
