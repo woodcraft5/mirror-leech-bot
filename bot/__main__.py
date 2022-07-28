f109d4cfa1cd0a1efcfdb01e21b2bc1e31d85c12
WOODcraft Repo <72670667+woodcraft5@users.noreply.github.com>
2022-07-28 17:42:08 +0530

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -63,8 +63,8 @@ def start(update, context):
 Welcome | ✤◄ 𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭 ►✤ Bot is ✔️Ready
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
-        sendMarkup(start_string, context.bot, update.message, reply_markup)
         update.effective_message.reply_photo("https://telegra.ph/file/48fab66115573350043b5.jpg", parse_mode=ParseMode.MARKDOWN)
+        sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
         sendMarkup('Sorry, You cannot use me', context.bot, update.message, reply_markup)
 
