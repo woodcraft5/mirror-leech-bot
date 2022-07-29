f24acfb8c0c5166aa98ee8192040a457b229ee64
woodcraft5 <sekhfaruk5@gmail.com>
2022-07-30 00:59:26 +0530

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -63,7 +63,7 @@ def start(update, context):
 Welcome | ✤◄ 𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭 ►✤ Bot is ✔️Ready
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
-        update.effective_message.reply_photo("https://telegra.ph/file/d2d65936765c436fa8835.jpg", start_string, parse_mode=ParseMode.MARKDOWN)
+        update.effective_message.reply_photo("https://telegra.ph/file/d2d65936765c436fa8835.jpg", start_string, context.bot, parse_mode=ParseMode.MARKDOWN)
         sendMarkup(start_string, context.bot, update.message)    
     else:
         sendMarkup('Sorry, You cannot use me', context.bot, update.message, reply_markup)
