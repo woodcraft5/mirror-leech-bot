08f795206f90b0c9e926f445e4e943ebb4ea0a46
woodcraft5 <sekhfaruk5@gmail.com>
2022-07-29 23:55:22 +0530

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -63,9 +63,10 @@ def start(update, context):
 Welcome | ✤◄ 𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭 ►✤ Bot is ✔️Ready
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
-        start_string, update.message, reply_markup, update.effective_message.reply_photo(IMAGE_URL, context.bot, parse_mode=ParseMode.MARKDOWN)           
+        update.effective_message.reply_photo(IMAGE_URL, context.bot, parse_mode=ParseMode.MARKDOWN)
+        sendMarkup(start_string, context, update.message, reply_markup)         
     else:
-        sendMarkup('Sorry, You cannot use me', context.bot, update.message, reply_markup)
+        sendMarkup('Sorry, You cannot use me', context.bot update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
