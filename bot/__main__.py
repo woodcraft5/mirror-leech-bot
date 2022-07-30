eb81103061eb3cead99c7c8ebdb459257f95b7af
woodcraft5 <sekhfaruk5@gmail.com>
2022-07-30 11:51:04 +0530

    __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -48,7 +48,8 @@ def stats(update, context):
             f'<b>★RAM Usage ●</b> <code>{mem_p}%</code>\n'\
             f'<b>★</b>\n'        
     if heroku := getHerokuDetails(HEROKU_API_KEY, HEROKU_APP_NAME):
-        stats += heroku   
+        stats += heroku
+        update.effective_message.reply_photo(IMAGE_URL, stats, parse_mode=ParseMode.MARKDOWN)   
     reply_message = sendMessage(stats, context.bot, update.message)
     Thread(target=auto_delete_message, args=(context.bot, update.message, reply_message)).start()
 
