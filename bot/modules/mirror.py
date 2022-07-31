be9f7d7e110067287e57a3f435e819763874260a
woodcraft5 <sekhfaruk5@gmail.com>
2022-08-01 04:37:05 +0530

    mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -224,7 +224,7 @@ class MirrorListener:
             msg += f'\n<b>➦ Total Files ●</b>{folders}'
             if typ != 0:
                 msg += f'\n<b>➦ Corrupted Files ●</b>{typ}'
-            msg += f'\n\n<b>➦ User ●</b>{self.tag} <b>'
+            msg += f'\n\n<b>➦ User ●</b>{self.tag}'
             msg += f'\n<b>➦ Successfully...</b>'
             msg += f'\n<b>➦ It Tooks ●</b> {get_readable_time(time() - self.message.date.timestamp())}'
             msg += f'\n\n<b>➦ Repo-By ✤ {TITLE_NAME}</b>'
@@ -245,7 +245,7 @@ class MirrorListener:
             if ospath.isdir(f'{DOWNLOAD_DIR}{self.uid}/{name}'):
                 msg += f'\n<b>➦ SubFolders ●</b>{folders}'
                 msg += f'\n<b>➦ Files ●</b>{files}'
-            msg += f'\n\n<b>➦ User ●</b>{self.tag} <b>'
+            msg += f'\n\n<b>➦ User ●</b>{self.tag}'
             msg += f'\n<b>➦ Successfully...</b>'
             msg += f'\n<b>➦ It Tooks ●</b> {get_readable_time(time() - self.message.date.timestamp())}'
             msg += f'\n\n<b>➦ Repo-By ✤ {TITLE_NAME}</b>'
