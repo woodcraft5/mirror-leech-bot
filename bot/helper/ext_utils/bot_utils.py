0f472cb0e80ce27cd12bf45db1023245be0297e3
woodcraft5 <sekhfaruk5@gmail.com>
2022-08-01 03:50:23 +0530

    bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -254,7 +254,7 @@ def get_readable_message():
         if len(msg) == 0:
             return None, None
         bmsg = f"\n<b>✫▬✫▬✫▬✫▬✫▬✫▬✫▬✫</b>"
-        bmsg += f"\n<b>✫   ●◄║ 𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭 ║►●  ✫</b>"
+        bmsg += f"\n<b>✫   ●◄║ 𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭 ║►● ✫</b>"
         bmsg += f"\n<b>✫▬✫▬✫▬✫▬✫▬✫▬✫▬✫</b>"
         bmsg += f"\n<b>➦ Disk ●</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
         bmsg += f"<b> | ➦ UPTM ●</b> {get_readable_time(time() - botStartTime)}"
