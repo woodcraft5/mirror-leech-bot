ff144722c69e897ecb13449f1209cd5b153427b2
woodcraft5 <sekhfaruk5@gmail.com>
2022-08-01 16:13:46 +0530

    clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -2,18 +2,18 @@ from random import SystemRandom
 from string import ascii_letters, digits
 from telegram.ext import CommandHandler
 from threading import Thread
-from time import sleep
+from time import time, sleep
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, deleteMessage, delete_all_messages, update_all_messages, sendStatusMessage
+from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, deleteMessage, delete_all_messages, update_all_messages, sendStatusMessage, auto_delete_message
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
 from bot import bot, dispatcher, LOGGER, CLONE_LIMIT, STOP_DUPLICATE, download_dict, download_dict_lock, Interval, BOT_PM, MIRROR_LOGS, FSUB, \
-                FSUB_CHANNEL_ID, CHANNEL_USERNAME, TITLE_NAME
+                FSUB_CHANNEL_ID, CHANNEL_USERNAME, TITLE_NAME, CHAT_ID, AUTO_MUTE
 from bot.helper.ext_utils.bot_utils import get_readable_file_size, is_gdrive_link, is_gdtot_link, new_thread, is_appdrive_link
 from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot, appdrive
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
-from telegram import InlineKeyboardMarkup, ParseMode
+from telegram import InlineKeyboardMarkup, ParseMode, ChatPermissions
 from bot.helper.telegram_helper.button_build import ButtonMaker
 
 def _clone(message, bot, multi=0):
@@ -44,11 +44,19 @@ def _clone(message, bot, multi=0):
             buttons.buildbutton("➦Click Here to Start Me", f"{botstart}")
             startwarn = f"Dear {uname},\n\n<b>I found that you haven't started me in PM (Private Chat) yet.</b>\n\nFrom now on i will give link and leeched files in PM and log channel only"
             message = sendMarkup(startwarn, bot, message, InlineKeyboardMarkup(buttons.build_menu(2)))
+            Thread(target=auto_delete_message, args=(bot, message, message)).start()
             return
 
     args = message.text.split()
     reply_to = message.reply_to_message
     link = ''
+
+    if AUTO_MUTE:
+        try:
+            bot.restrict_chat_member(chat_id=message.chat.id, user_id=message.from_user.id, until_date=int(time()) + 30, permissions=ChatPermissions(can_send_messages=False))
+        except Exception as e:
+            print(f'[MuteUser] Error: {type(e)} {e}')
+
     if len(args) > 1:
         link = args[1].strip()
         if link.strip().isdigit():
@@ -134,7 +142,7 @@ def _clone(message, bot, multi=0):
                     update_all_messages()
             except IndexError:
                 pass
-        cc = f'\n\n<b>➦ User </b>{tag}<b> ➦ Your file successfully...</b>\n\n<b>➦ Repo-By ✤ <i>{TITLE_NAME}</i></b>'
+        cc = f'\n\n<b>➦ User ●</b>{tag}<b> ➦ Successfully...</b>\n\n<b>➦ Repo-By ✤ <i>{TITLE_NAME}</i></b>'
         if button in ["cancelled", ""]:
             sendMessage(f"{tag} {result}", bot, message)
         else:
@@ -146,8 +154,33 @@ def _clone(message, bot, multi=0):
             if apdict.get('link_type') == 'login':
                 LOGGER.info(f"Deleting: {link}")
                 gd.deletefile(link)
+        if MIRROR_LOGS:	
+            try:	
+                for chatid in MIRROR_LOGS:	
+                    bot.sendMessage(chat_id=chatid, text=result + cc, reply_markup=button, parse_mode=ParseMode.HTML)	
+            except Exception as e:	
+                LOGGER.warning(e)	
+        if BOT_PM and message.chat.type != 'private':	
+            try:	
+                bot.sendMessage(message.from_user.id, text=result, reply_markup=button,	
+                                parse_mode=ParseMode.HTML)	
+            except Exception as e:	
+                LOGGER.warning(e)	
+                return
     else:
-        sendMessage('Send Gdrive or GDToT/AppDrive link along with command or by replying to the link by command', bot, message)
+        if AUTO_MUTE:
+            try:
+                uname = message.from_user.mention_html(message.from_user.first_name)
+                user = bot.get_chat_member(CHAT_ID, message.from_user.id)
+                if user.status not in ['creator', 'administrator']:
+                    bot.restrict_chat_member(chat_id=message.chat.id, user_id=message.from_user.id, until_date=int(time()) + 30, permissions=ChatPermissions(can_send_messages=False))
+                    return sendMessage(f"Dear {uname}️,\n\n<b>You are MUTED until you learn how to use me.\n\nWatch others or read </b>/{BotCommands.HelpCommand}", bot, message)
+                else:
+                    return sendMessage(f"OMG, {uname} You are a <b>Admin.</b>\n\nStill don't know how to use me!\n\nPlease read /{BotCommands.HelpCommand}", bot, message)
+            except Exception as e:
+                print(f'[MuteUser] Error: {type(e)} {e}')
+        else:
+            return sendMessage(f"Please enter a valid command.\nRead /{BotCommands.HelpCommand} and try again.", bot, message)
 
 @new_thread
 def cloneNode(update, context):
