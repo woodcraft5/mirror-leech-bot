aaad6cc564256bc2b2f6312e0c659e9819e6b8a7
woodcraft5 <sekhfaruk5@gmail.com>
2022-08-01 16:09:38 +0530

    mirror_status.py

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -15,8 +15,8 @@ def mirror_status(update, context):
             currentTime = get_readable_time(time() - botStartTime)
             free = get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)
             message = '🌼 Nothing To Do ! 🌼\n✧✧✧𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭✧✧✧'
-            message += f"\n\n<b>➥ CPU ➥    :</b> {cpu_percent()}%\n<b>➥ SSD ➥     :</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}" \
-                       f"\n<b>➥ RAM ➥     :</b> {virtual_memory().percent}%\n<b>➥ UPTM ➥   :</b> {get_readable_time(time() - botStartTime)}"
+            message += f"\n\n<b>➥ CPU ➥    ●</b> {cpu_percent()}%\n<b>➥ SSD ➥     ●</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}" \
+                       f"\n<b>➥ RAM ➥     ●</b> {virtual_memory().percent}%\n<b>➥ UPTM ➥   ●</b> {get_readable_time(time() - botStartTime)}"
             reply_message = sendMessage(message, context.bot, update.message)
             Thread(target=auto_delete_message, args=(context.bot, update.message, reply_message)).start()
     else:
@@ -40,7 +40,7 @@ def status_pages(update, context):
     query = update.callback_query
     with status_reply_dict_lock:
         if not status_reply_dict or not Interval or time() - list(status_reply_dict.values())[0][1] < 2:
-            query.answer(text="Wait One More Second!", show_alert=True)
+            query.answer(text="Try again later...", show_alert=True)
             return
     data = query.data
     data = data.split()
